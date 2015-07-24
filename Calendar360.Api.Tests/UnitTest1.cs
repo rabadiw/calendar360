1b4063cbd6f2154e628b123c91bddb0f9aee9c36
Wael Rabadi <waelra@microsoft.com>
2015-07-24 16:25:03 -0500

    Initial - consist of project skeleton

---
+++ Calendar360.Api.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calendar360.Api.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Calendar360.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Calendar360.Api.WebApiApplication" Language="C#" %>
